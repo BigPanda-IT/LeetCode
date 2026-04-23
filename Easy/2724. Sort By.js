b61d90c515f41b5b35e84c6c36ce8dd426257508
Elena <lena_bondareva_03@mail.ru>
2026-04-23 15:00:53 +0300

    Adding the script of task from LeetCode

---
+++ Easy/2724. Sort By.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    
+    sortedArr=arr.sort((a, b) => fn(a)-fn(b));
+
+    return sortedArr;
+};
