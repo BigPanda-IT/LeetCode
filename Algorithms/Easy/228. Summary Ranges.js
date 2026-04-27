6d853b33eeaaa87846f76da20e852c4859223875
Elena <lena_bondareva_03@mail.ru>
2026-04-27 13:57:28 +0300

    Adding the script of task from LeetCode

---
+++ Algorithms/Easy/228. Summary Ranges.js
@@ -0,0 +1,26 @@
+**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    let res=[];
+    let i=0;
+
+    while (i<nums.length) {
+        let start = i;
+
+        while (i+1<nums.length && nums[i]+1 == nums[i+1]) {
+            i++;
+        }
+
+        if (start === i) {
+            res.push(nums[start].toString());
+        }
+        else {
+            res.push(nums[start] + "->" + nums[i]);
+        }
+        i++;
+    }
+    
+    return res;
+};
