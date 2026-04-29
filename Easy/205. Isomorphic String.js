ac94716fd25acd0b0d29f73a3073c1bfa937bcfe
Elena <lena_bondareva_03@mail.ru>
2026-04-29 13:00:25 +0300

    Adding the script of task from LeetCode

---
+++ Easy/205. Isomorphic String.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const dictionary1 = {};
+    const dictionary2 = {};
+
+    let i=0;
+    let j=0;
+
+    while (i<s.length && j<t.length) {
+        if (dictionary1[s[i]] !== undefined &&  dictionary1[s[i]] != t[j]) {
+            return false;
+        }
+        else {
+
+            if (dictionary2[t[j]] !== undefined &&  dictionary2[t[j]] != s[i]) {
+                return false;
+            }
+            else {
+                dictionary1[s[i]] = t[j];
+                i++;
+                j++;
+            }
+        }
+    }
+
+    return true;
+
+};
