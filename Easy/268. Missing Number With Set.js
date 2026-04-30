a918a532901fcdff8b8de2c79ee70a01d023e6d7
Elena <lena_bondareva_03@mail.ru>
2026-04-30 15:29:42 +0300

    Add files via upload

---
+++ Easy/268. Missing Number With Set.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const set = new Set(nums);
+
+    for (let i=0; i<nums.length+1; i++) {
+        if (set.has(i) == false) {
+            return i;
+        }
+    }
+};
