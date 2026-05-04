423f4595cc547c3747b023ab660eb4b7e546e0bf
Elena <lena_bondareva_03@mail.ru>
2026-05-04 20:41:35 +0300

    Adding the script of task from LeetCode

---
+++ Easy/136. Single Number With Sort.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+
+    nums.sort((a,b) => a - b);
+
+    console.log(nums);
+
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] !== nums[i+1]) {
+            return nums[i];
+        }
+        else {
+            i++;
+        }
+    }
+};
