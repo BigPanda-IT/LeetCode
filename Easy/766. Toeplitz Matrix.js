8e9a0dcf6e2b137b49e11ea370b10864b3b29fa2
Elena <lena_bondareva_03@mail.ru>
2026-05-08 13:03:03 +0300

    Adding the script of task from LeetCode

---
+++ Easy/766. Toeplitz Matrix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+
+    let k=0;
+    let m = matrix.length;
+    let n = matrix[k].length;
+
+    for (let i=0; i<m-1; i++) {
+        for (let j=0; j<n-1; j++) {
+            if (matrix[i][j] !== matrix[i+1][j+1]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
