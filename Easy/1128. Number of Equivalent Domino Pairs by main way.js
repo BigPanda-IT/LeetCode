0a545c300e274be4b7c048a856266932b1b63d63
Elena <lena_bondareva_03@mail.ru>
2026-05-25 23:05:02 +0300

    Adding the script of task from LeetCode

---
+++ Easy/1128. Number of Equivalent Domino Pairs by main way.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} dominoes
+ * @return {number}
+ */
+var numEquivDominoPairs = function (dominoes) {
+    let map = new Map();
+
+    let counter=0;
+
+    for (let i=0; i<dominoes.length; i++) {
+        dominoes[i].sort();
+    }
+
+    for (let i=0; i<dominoes.length; i++) {
+        let val = JSON.stringify(dominoes[i]);
+        let curr =  map.get(val) ?? 0;
+        map.set(val, curr + 1);
+    }
+
+    for (let [key, value] of map) {
+        if (value > 1) {
+            counter += (value * (value - 1)) / 2;
+        }
+    }
+
+    return counter;
+};
